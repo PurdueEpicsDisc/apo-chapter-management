872dc86040db83c249d38e42103501a2ef197b74
cnguyenm <cnguyenm@purdue.edu>
2016-11-15 23:16:07 +0000

    fix home page

--- app/server/main.js
+++ app/server/main.js
@@ -4,13 +4,3 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-
-// if (Meteor.isServer) {
-    
-//     AdminConfig = {
-//       collections: {
-//         Events: {}
-//       }
-// };
-    
-// }
